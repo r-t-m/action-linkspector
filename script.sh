79b40b82ecd2e2ab0b870430e1e1ee8ece9975f2
Artem Chubar <57397719+r-t-m@users.noreply.github.com>
2025-06-23 14:31:11 +0000

    chore: enable debug

--- script.sh
+++ script.sh
@@ -1,11 +1,20 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+set -ex
 
 if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+handle_error() {
+    local exit_code=$?
+    if [ -s "$tempfile" ]; then
+        echo "Error: $(cat "$tempfile")"
+    fi
+    rm -f "$tempfile" 2>/dev/null
+    exit "$exit_code"
+}
 
 echo '::group::🔗💀 Installing linkspector ... https://github.com/UmbrellaDocs/linkspector'
 npm install -g @umbrelladocs/linkspector@0.4.5
@@ -23,14 +32,22 @@ fi
 echo '::endgroup::'
 
 echo '::group:: Running linkspector with reviewdog 🐶 ...'
-linkspector check -c "${INPUT_CONFIG_FILE}" -j |
-  reviewdog -f=rdjson \
+trap 'handle_error' ERR
+tempfile=$(mktemp)
+linkspector_output=$(linkspector check -c "${INPUT_CONFIG_FILE}" -j 2> "$tempfile")
+
+tempfile=$(mktemp)
+reviewdog_output=$(echo "$linkspector_output" 2> "$tempfile" | reviewdog -f=rdjson \
     -name="${INPUT_TOOL_NAME}" \
     -reporter="${INPUT_REPORTER}" \
     -filter-mode="${INPUT_FILTER_MODE}" \
     -fail-level="${INPUT_FAIL_LEVEL}" \
     -level="${INPUT_LEVEL}" \
-    "${INPUT_REVIEWDOG_FLAGS}"
+    "${INPUT_REVIEWDOG_FLAGS}")
+exit_code=$?
+
+echo "$linkspector_output"
+echo "$reviewdog_output"
 exit_code=$?
 echo '::endgroup::'
 
